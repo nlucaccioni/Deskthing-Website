2ce0aea2b24f899b5e0d2948a59c37e69887028c
ItsRiprod <riprodgames@gmail.com>
2024-07-29 18:17:21 -0400

    Updated with buttons and tailwind

--- deskthingsite/app/routes/_index.tsx
+++ deskthingsite/app/routes/_index.tsx
@@ -9,40 +9,63 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
+    <div className="font-geist p-4 w-screen h-screen bg-slate-800 flex flex-col items-center justify-center">
+      <div>
+        <p className="text-white">The</p>
+        <h1 className="font-Wingding text-white text-5xl">DESKTHING</h1>
+      </div>
+      <div className="flex flex-wrap pt-5 items-center justify-center gap-4">
           <a
-            className="text-blue-700 underline visited:text-purple-900"
+            className="border border-amber-600 p-3 hover:bg-amber-600 rounded-xl text-white"
             target="_blank"
-            href="https://remix.run/start/quickstart"
+            href="https://www.reddit.com/r/DeskThing/"
             rel="noreferrer"
           >
-            5m Quick Start
+            REDDIT
           </a>
-        </li>
-        <li>
           <a
-            className="text-blue-700 underline visited:text-purple-900"
+            className="border border-indigo-600 p-3 hover:bg-indigo-600 rounded-xl text-white"
             target="_blank"
-            href="https://remix.run/start/tutorial"
+            href="https://www.reddit.com/r/DeskThing/"
             rel="noreferrer"
           >
-            30m Tutorial
+            DISCORD
           </a>
-        </li>
-        <li>
           <a
-            className="text-blue-700 underline visited:text-purple-900"
+            className="border border-zinc-600 p-3 hover:bg-zinc-600 rounded-xl text-white"
             target="_blank"
-            href="https://remix.run/docs"
+            href="https://www.reddit.com/r/DeskThing/"
             rel="noreferrer"
           >
-            Remix Docs
+            GITHUB
           </a>
-        </li>
-      </ul>
+          <a
+            className="border border-slate-300 p-3 hover:bg-slate-300 rounded-xl text-white"
+            target="_blank"
+            href="https://www.reddit.com/r/DeskThing/"
+            rel="noreferrer"
+          >
+            TRELLO
+          </a>
+          <a
+            className="border border-fuchsia-600 p-3 hover:bg-fuchsia-600 rounded-xl text-white"
+            target="_blank"
+            href="https://www.reddit.com/r/DeskThing/"
+            rel="noreferrer"
+          >
+            SUPPORT
+          </a>
+          <a
+            className="border border-red-600 p-3 hover:bg-red-600 rounded-xl text-white"
+            target="_blank"
+            href="https://www.reddit.com/r/DeskThing/"
+            rel="noreferrer"
+          >
+            YOUTUBE
+          </a>
+
+      </div>
+      
     </div>
   );
 }
