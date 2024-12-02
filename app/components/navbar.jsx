9bcb95978b25b185963791b242e2fc0414b8e990
nlucaccioni <n.lucaccioni@gmail.com>
2024-12-02 10:50:36 -0600

    Style navbar. Add logo svg. Remove unused font files.

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,7 +1,10 @@
+import LogoSVG from "../assets/Logo";
+import { Github } from 'lucide-react';
+
 
 function NavLink({ href, label}) {
     return (
-        <a href={href} className="py-1 px-2 rounded-md trainsition ease-in-out duration-200 text-neutral-300 hover:text-neutral-50 hover:bg-neutral-50/10" >{label}</a>
+        <a href={href} className="text-base py-1 px-2 rounded-md trainsition ease-in-out duration-200 text-neutral-300 hover:text-neutral-50 hover:bg-neutral-50/10" >{label}</a>
     )
 };
 
@@ -19,12 +22,15 @@ export default function Navbar() {
     )
     
     return (
-        <nav className="flex flex-row justify-between py-3 px-6 border-b border-neutral-800 bg-neutral-950/80">
+        <nav className="flex flex-row justify-between items-center py-3 px-6 border-b border-neutral-800 bg-neutral-950/80">
             <div className="flex flex-row gap-5">
-                <h1>DESKTHING</h1>
-                <ul className="flex flex-row gap-3">{navList}</ul>
+                <a href="/"><LogoSVG /></a>
+                <ul className="flex flex-row items-center gap-3">{navList}</ul>
             </div>
-            <a>github</a>
+            <a href="https://github.com/ItsRiprod/DeskThing" 
+            className="transition ease-in-out duration-200 text-neutral-50 hover:text-green-600">
+                <Github />
+                </a>
         </nav>
     )
 }
