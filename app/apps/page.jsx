58ba8b5ac5f51eafa86aceecbf77aefb12adaf48
nlucaccioni <n.lucaccioni@gmail.com>
2024-12-02 23:33:08 -0600

    Fill empty pages with placeholders

---
+++ app/apps/page.jsx
@@ -0,0 +1,7 @@
+export default function AppPage() {
+  <>
+    <div className="min-h-svh flex flex-row justify-between pt-nav">
+      <div className="supportersContainer flex flex-col mx-auto gap-columnGap items-center"></div>
+    </div>
+  </>;
+}
