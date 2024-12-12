a337536e892edb83f6bbedb1bc9d2efa78a278ef
nlucaccioni <n.lucaccioni@gmail.com>
2024-12-12 14:04:06 -0600

    fix typescript error

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import React from 'react';
 
 
 export const metadata = {
