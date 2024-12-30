1f8b55ce7680f3e0da19c8c5eabb05d14d5c4a9e
nlucaccioni <n.lucaccioni@gmail.com>
2024-12-30 12:40:55 -0600

    Remove ! from Get Started on home page

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ const HomePage: FC = async () => {
                     your productivity in the process. Everyone wins.
                   </p>
                   <div className="flex flex-col sm:flex-row flex-wrap gap-4">
-                    <BtnArrow to={btnLinks.getStarted} label="Get Started!" filled />
+                    <BtnArrow to={btnLinks.getStarted} label="Get Started" filled />
                     <BtnArrow to={btnLinks.wiki} label="Documentation" />
                   </div>
                 </div>
