7a283c5cc3bdedea4d9c9687d5d6bbd4f443ac92
nlucaccioni <n.lucaccioni@gmail.com>
2024-12-30 11:57:26 -0600

    Fix Apps page xl breakpoint

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -36,7 +36,7 @@ export default async function AppPage(): Promise<JSX.Element> {
   return (
     <>
       <div className="min-h-svh flex flex-row justify-between pt-nav mx-6 xl:mx-0">
-        <div className="wideContainer flex flex-col mx-auto gap-columnGap items-center ">
+        <div className="wideContainer xl:px-6 flex flex-col mx-auto gap-columnGap items-center ">
           <section className="w-full flex flex-col gap-4">
             <h2>Official Apps</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
